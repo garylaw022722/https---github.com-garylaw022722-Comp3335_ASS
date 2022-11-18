53a7bc40ba4d17e1ef1df334ca1b54a826ebf093
law <law190444956@gmail.com>
2022-11-18 23:10:57 +0800

    sas

--- src/view/HRStaff/createEmployee.php
+++ src/view/HRStaff/createEmployee.php
@@ -1,9 +1,10 @@
-<!-- HR Staff - employee create--->
 <?php
-session_start();
+
 include('../../php/db_Connection.php');
+session_start();
 
 $uuid =  $_SESSION["id"];
+
 if (isset(
     $_POST["user_id"],
     $_POST["Dept_id"],
@@ -17,7 +18,8 @@ if (isset(
     $_POST["tel"]
 )) {
     // data from create employee form
-    $user_id = $_POST["user_id"];
+    $conn = getConnection(json_decode($_SESSION[$uuid]));
+    $userID = $_POST["user_id"];
     $Dept_id = $_POST["Dept_id"];
     $firstName = $_POST["firstName"];
     $lastName = $_POST["lastName"];
@@ -27,23 +29,15 @@ if (isset(
     $address = $_POST["address"];
     $ID_Card_No = $_POST["ID_Card_No"];
     $tel = $_POST["tel"];
+    
 
-    $conn = getConnection(json_decode($_SESSION[$uuid]));
-    $sql = "INSERT INTO comp3335.Employee(user_id,Dept_id,firstName,lastName,BOD,salary,gender,address,ID_Card_No,tel) values (?,?,?,?,?,?,?,?,?,?)";
+    $sql = 'INSERT INTO comp3335.Employee(user_id,Dept_id,firstName,lastName,BOD,salary,gender,address,ID_Card_No,tel) values (?,?,?,?,?,?,?,?,?,?)';
     $preState = $conn->prepare($sql);
-    $preState->bind_param("sisssissss", $user_id, $Dept_id, $firstName, $lasbtName, $BOD, $salary, $gender, $address, $ID_Card_No, $tel);
-    $preState->execute();
+    $preState->bind_param("sisssissss", $userID, $Dept_id, $firstName, $lasbtName, $BOD, $salary, $gender, $address, $ID_Card_No, $tel);
+  
 
-    //$sql = "INSERT INTO comp3335.Employee 
-    //    VALUES ('$user_id', '$Dept_id', '$firstName', '$lastName', '$BOD', '$salary', '$gender', '$address', '$ID_Card_No', '$tel')";
-
-    if ($preState == TRUE) {
-        header("Location: /main.php?msg=" . urlencode("Employee create successfully."));
-    } else {
-        header("Location: /main.php?msg=" . urlencode("Error occured."));
-    }
-} else {
-    header("Location: /main.php?msg=" . urlencode("Missing Data."));
+    $sql = "INSERT INTO comp3335.Employee 
+       VALUES ('$userID', '$Dept_id', '$firstName', '$lastName', '$BOD', '$salary', '$gender', '$address', '$ID_Card_No', '$tel')";
 }
 
 ?>
