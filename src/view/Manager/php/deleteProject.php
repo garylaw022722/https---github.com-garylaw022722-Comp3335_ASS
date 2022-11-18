490d58ad4063f8117b17b46429125a2d31c32051
Matt <yukchunma5@gmail.com>
2022-11-18 13:47:08 +0800

    Matt Manager

--- src/view/Manager/php/deleteProject.php
+++ src/view/Manager/php/deleteProject.php
@@ -1,13 +1,19 @@
 <?php
-include('../../../php/db_connect.php');
+include('../../../php/db_Connection.php');
+session_start();
+$uuid =  $_SESSION["id"];
 
 if(isset($_POST['project_id'])){
 
+    $conn = getConnection(json_decode($_SESSION[$uuid])); 
+    //htmlxxx
+    $sql = "DELETE FROM Project WHERE project_id= ? LIMIT 1";
+    $preState =$conn->prepare($sql);
     $project_id = $_POST['project_id'];
-// sql to delete a record
-$sql = "DELETE FROM Project WHERE project_id='$project_id'";
+  $preState->bind_param("s",$project_id);
+  $preState->execute();
 
-if ($con->query($sql) === TRUE) {
+if ($preState == true) {
     header("Location:../main.php");
 } else {
     header( "Location:../main.php?msg=".urlencode("Wrong data format"));
